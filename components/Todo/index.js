f07e20333a89da3d056da764bfcb1e2644c918eb
silviolimeira <sicemal@gmail.com>
2023-04-29 09:10:22 -0300

    Adding A First Page

---
+++ components/Todo/index.js
@@ -0,0 +1,7 @@
+export default function Todo() {
+  return (
+    <div>
+      <h1>Todo 1</h1>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import Todo from "../components/Todo";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div className={styles.container}>
       <h1>Hello Next World!</h1>
+      <Todo />
     </div>
-  )
+  );
 }
