1f5ce94450439ec33451db3e003c7f589ec98db2
silviolimeira <sicemal@gmail.com>
2023-04-29 09:20:35 -0300

    Working with Nested Paths & Routes

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <div>
-      <h1>The About Page</h1>
+      <h1>The About Page in about folder</h1>
     </div>
   );
 }
---
+++ pages/portfolio/index.js
@@ -0,0 +1,7 @@
+export default function PortifolioPage() {
+  return (
+    <div>
+      <h1>Portifolio Page</h1>
+    </div>
+  );
+}
