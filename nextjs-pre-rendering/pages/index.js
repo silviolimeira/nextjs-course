98b7b1c7eac6d0f3b6cc034f3529e202f8a8f8da
silviolimeira <sicemal@gmail.com>
2023-05-16 11:44:37 -0300

    Adding getStaticProps To Pages

--- nextjs-pre-rendering/pages/index.js
+++ nextjs-pre-rendering/pages/index.js
@@ -1,10 +1,22 @@
 import App from "../components/App";
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <>
-      <App />
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product AA 1" }],
+    },
+  };
 }
 
 export default HomePage;
