ef93f66fde62bfd5a9e6be83904ab3613300ca19
silviolimeira <sicemal@gmail.com>
2023-05-18 08:21:31 -0300

    Working With Fallback Pages

--- nextjs-pre-rendering/pages/[pid].js
+++ nextjs-pre-rendering/pages/[pid].js
@@ -4,6 +4,10 @@ import fs from "fs/promises";
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
 
+  //if (!loadedProduct) {
+  //  return <p>Loading ...</p>;
+  //}
+
   return (
     <>
       <h1>{loadedProduct.title}</h1>
@@ -33,7 +37,7 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
   return {
     paths: [{ params: { pid: "p1" } }, { params: { pid: "p2" } }],
-    fallback: false,
+    fallback: "blocking", //true,
   };
 }
 
