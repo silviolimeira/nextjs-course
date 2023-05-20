b6a666eb6943e13ab822b7e313579b634715457b
silviolimeira <sicemal@gmail.com>
2023-05-20 12:09:23 -0300

    Using the useSWR NextJS Hook

--- nextjs-pre-rendering/pages/last-sales.js
+++ nextjs-pre-rendering/pages/last-sales.js
@@ -1,26 +1,38 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("http://172.21.64.1:8080/sales")
-      .then((response) => response.json())
+  const fetcher = (...args) =>
+    fetch(...args)
+      .then((response) => {
+        setIsLoading(true);
+        return response.json();
+      })
       .then((data) => {
         setSales(data.content);
         setIsLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-  }, []);
+
+  const { error } = useSWR("http://172.21.64.1:8080/sales", fetcher);
 
   if (isLoading) {
     return <p>Loading ...</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
+  if (error) {
+    return <p>Failed to load.</p>;
   }
+
+  if (!sales) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <ul>
       <h1>Last Sales</h1>
